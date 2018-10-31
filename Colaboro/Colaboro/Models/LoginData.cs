f7f55bbcabf33897ff376c65286d2738ead7b51f
insinfo <insinfo2008@gmail.com>
2018-10-31 00:54:32 -0300

    sdf

---
+++ Colaboro/Colaboro/Models/LoginData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Colaboro.Models
+{   
+    public class LoginData
+    {
+        public string userName { get; set; } = "";
+        public string password { get; set; } = "";
+        public string ipPrivado { get; set; } = "";
+        public string ipPublico { get; set; } = "";
+    }
+}
