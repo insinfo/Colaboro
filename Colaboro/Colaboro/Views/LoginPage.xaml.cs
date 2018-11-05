5702e4d4e00041c0599f85efa4da4ab5ebcb4568
insinfo <insinfo2008@gmail.com>
2018-11-05 17:17:43 -0300

    teste GTK e WPF
    teste GTK e WPF

--- Colaboro/Colaboro/Views/LoginPage.xaml.cs
+++ Colaboro/Colaboro/Views/LoginPage.xaml.cs
@@ -2,6 +2,7 @@
 using Colaboro.Services;
 using Newtonsoft.Json;
 using System;
+using System.Diagnostics;
 using System.Net.Http;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -37,32 +38,27 @@ namespace Colaboro.Views
                 passwordEntry.Focus();
                 return;
             }
+   
 
-            string page = "http://www.google.com/";     
-            HttpClient client = new HttpClient();
-            HttpResponseMessage response = await client.GetAsync(page);
-            HttpContent content = response.Content;      
-            string result = await content.ReadAsStringAsync();
-            Utils.ShowAlert(this, result);               
-                
-            
             //await this.Navigation.PushModalAsync(new MainPage());
 
             //var dialog = UserDialogs.Instance;//.Loading("Carregando...",null,null,false);
             //dialog.ShowLoading();                     
-            /*
+            
               RestClient rest = new RestClient();
               rest.DataToSender = new { userName = usernameEntry.Text, password = passwordEntry.Text };
               rest.SetMethodPOST();
               rest.ErrorCallbackFunction = (res) => {
                   // Utils.ShowAlert(this, res);
                   formValidationInfoLabel.Text = res;
+                  Debug.WriteLine(res);
               };
               rest.SuccessCallbackFunction = (res) => {
                   formValidationInfoLabel.Text = res;
+                  Debug.WriteLine(res);
                   /// Navigation.PushModalAsync(new MainPage());
               };           
-              await rest.Exec("/api/auth/login");*/
+              await rest.Exec("/api/auth/login");
             // dialog.HideLoading();
             //dialog.Alert(resp);
 
